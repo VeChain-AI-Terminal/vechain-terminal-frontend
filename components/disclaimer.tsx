056846e019271350a348c45e94b36761633b5acb
lvmShantanu <shantanu@lvmodel.com>
2025-08-16 00:07:26 +0530

    disclaimer updtae

--- components/disclaimer.tsx
+++ components/disclaimer.tsx
@@ -4,8 +4,8 @@ export default function Disclaimer() {
   return (
     <div className="text-xs text-muted-foreground text-center w-full">
       <p>
-        Orange Terminal &#40;beta&#41;: Search on-chain. Send tokens. Not
-        financial advice — <span className="text-theme-orange">DYOR</span>.
+        Orange Terminal (beta): Search on-chain. Stake. Earn. Not financial
+        advice — <span className="text-theme-orange">DYOR</span>.
       </p>
       {/* <p>It is NOT a financial advisor.</p> */}
     </div>
